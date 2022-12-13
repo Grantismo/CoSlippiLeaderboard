05dbf6f7b1ca926d296f3af666a5b6f80b1beede
Grant Warman <grant.warman@gmail.com>
2022-12-13 13:14:34 -0700

    Add fetch request to pull ranked data

--- src/components/routes/home/HomePage.tsx
+++ src/components/routes/home/HomePage.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button } from '@mantine/core';
+import { getPlayerData } from '../../../lib/slippi'
 
 export default function HomePage() {
+  useEffect(() => {
+    getPlayerData('BLRP#745').then((data) => console.log(data))
+  }, [])
+
   return (
     <div>
-      <h1 style={{ fontSize: 50, fontWeight: 'bold', textAlign: 'center' }}>
-        React Pages Boilerplate
+      <h1>
+        Colorado Melee Leaderboard
       </h1>
 
       <Button>A button</Button>
---
+++ src/lib/slippi.ts
@@ -0,0 +1,54 @@
+export const getPlayerData = async (connectCode: string) => {
+  const query = `fragment userProfilePage on User {
+    displayName
+    connectCode {
+          code
+          __typename
+        }
+      rankedNetplayProfile {
+            id
+            ratingOrdinal
+            ratingUpdateCount
+            wins
+            losses
+            dailyGlobalPlacement
+            dailyRegionalPlacement
+            continent
+            characters {
+                    id
+                    character
+                    gameCount
+                    __typename
+                  }
+            __typename
+          }
+      __typename
+  }
+
+  query AccountManagementPageQuery($cc: String!, $uid: String!) {
+      getUser(fbUid: $uid) {
+            ...userProfilePage
+            __typename
+          }
+      getConnectCode(code: $cc) {
+            user {
+                    ...userProfilePage
+                    __typename
+                  }
+            __typename
+          }
+  }`;
+
+  const req = await fetch('https://gql-gateway-dot-slippi.uc.r.appspot.com/graphql', {
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      operationName: 'AccountManagementPageQuery',
+      query,
+      variables: { cc: connectCode, uid: connectCode },
+    }),
+    method: 'POST',
+  });
+  return req.json();
+};
