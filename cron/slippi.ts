e91a8ef45c4e6ea4b740fe4bfcfa36e89097b501
Grant Warman <grant.warman@gmail.com>
2022-12-14 12:32:27 -0700

    reduce to 1 qps

--- cron/slippi.ts
+++ cron/slippi.ts
@@ -55,8 +55,7 @@ export const getPlayerData = async (connectCode: string) => {
   return req.json();
 };
 
-// limit to 3 QPS
-const limiter = new RateLimiter({tokensPerInterval: 3, interval: 'second'})
+const limiter = new RateLimiter({tokensPerInterval: 1, interval: 'second'})
 
 export const getPlayerDataThrottled = async (connectCode: string) => {
   const remainingRequests = await limiter.removeTokens(1);
