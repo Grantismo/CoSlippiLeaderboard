e0057bc70fe56a78d3a5405d764fd0ef4a64e131
Oracle Public Cloud User <opc@co-melee.subnet12201615.vcn12201615.oraclevcn.com>
2022-12-20 22:07:40 +0000

    local node path update

--- cron/run.sh
+++ cron/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-NODE_BIN=/usr/local/bin/node
+NODE_BIN=/home/opc/.nvm/versions/node/v18.12.0/bin/node
 cd "$DIR_PATH/.." 
 "$NODE_BIN" --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
