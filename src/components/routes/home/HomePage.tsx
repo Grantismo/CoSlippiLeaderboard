66d572274f2db8a9e63b6b9142adba53985b9628
Grant Warman <grant.warman@gmail.com>
2023-06-29 15:57:32 -0600

    Add flag of colorado

--- src/components/routes/home/HomePage.tsx
+++ src/components/routes/home/HomePage.tsx
@@ -7,6 +7,7 @@ import timestamp from '../../../../cron/data/timestamp.json';
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime' // import plugin
 import * as settings from '../../../../settings'
+import ColoradoFlag from '../../../../images/Flag_of_Colorado.svg';
 dayjs.extend(relativeTime)
 
 
@@ -53,6 +54,7 @@ export default function HomePage() {
 
   return (
     <div className="flex flex-col items-center h-screen p-8">
+      <img className="h-48" src={ColoradoFlag} alt="colorado flag" />
       <h1 className="text-3xl m-4 text-center text-white">
         {settings.title}
       </h1>
