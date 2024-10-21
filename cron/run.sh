2087d255ef5a92c36ea2e510446e8e0e82110b86
Grant Warman <grant.warman@gmail.com>
2024-10-21 16:24:33 -0600

    setup env

--- cron/run.sh
+++ cron/run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash -l
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$DIR_PATH/.." 
+
+mkdir -p cron/data
+if [ ! -f cron/data/players-new.json ]; then
+  echo '[]' >> cron/data/players-new.json
+fi
+mkdir -p cron/logs
+if [ ! -f cron/logs/log.txt ]; then
+  touch cron/logs/log.txt
+fi
+
 node --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
